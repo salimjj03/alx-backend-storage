d921081cfe90df0e6cb0450df107b92b091643b4
salim aliyu <salemaliyu1882@gmail.com>
2024-05-11 09:34:53 +0000

    sql trigger

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,7 @@
+-- decreases the quantity of an item after adding a new order.
+CREATE trigger decreases
+AFTER INSERT ON orders
+FOR EACH ROW
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name = NEW.item_name;
