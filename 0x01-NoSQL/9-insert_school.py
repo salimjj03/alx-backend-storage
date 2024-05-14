3a717a041881a520af169db22368fae986106dcc
salim aliyu musawa <salemaliyu1992@gmail.com>
2024-05-14 14:37:48 -0700

    insert many

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -9,4 +9,4 @@ def insert_school(mongo_collection, **kwargs):
     function that inserts a new document in a collection based on kwargs
     """
 
-    return mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert_one(kwargs).inserted_id
